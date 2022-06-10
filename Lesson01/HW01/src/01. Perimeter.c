2ded874fb303af6f7236bc2cb1bd07021e8fc08c
vanhan <106876883+halcyonaru@users.noreply.github.com>
2022-06-10 19:06:25 +0700

    Add files via upload

--- Lesson01/HW01/src/01. Perimeter.c
+++ Lesson01/HW01/src/01. Perimeter.c
@@ -2,19 +2,21 @@
 int tinhChuViHinhChuNhat(int chieudai, int chieurong) {
 	int chuvi;
 	chuvi = 2*(chieudai + chieurong);
-	printf("Chu vi hinh chu nhat co canh (%d, %d) la: %d", chieudai, chieurong, chuvi);
+	printf("Chu vi hinh chu nhat co canh (%d, %d) la: %d\n", chieudai, chieurong, chuvi);
 	return chuvi;
 }
 
 int tinhChuViTamGiac(int canhA, int canhB, int canhC) {
 	int chuvi3;
 	chuvi3 = canhA + canhB + canhC;
-	printf("\nChu vi hinh tam giac co canh (%d, %d, %d): %d", canhA, canhB, canhC, chuvi3);
+	printf("Chu vi hinh tam giac co canh (%d, %d, %d): %d\n", canhA, canhB, canhC, chuvi3);
 	return chuvi3;
 }
 
 int main() {
 	int chuvihcn = tinhChuViHinhChuNhat(8, 6);
+	int chuvihinhchunhat = tinhChuViHinhChuNhat(5, 3);
 	int chuvitg = tinhChuViTamGiac(3, 4, 5);
+	int chuvitamgiac = tinhChuViTamGiac(4, 5, 6);
 	return 0;
 }
